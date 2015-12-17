31f4cbe3720dc639a9403a1b5271d3c2c47dde5a
David Whetstone <david@humblehacker.com>
2015-12-17 10:14:58 -0800

    add lastStatus property
    It can be useful when a keychain operation fails to be able to access why it failed.

--- Lockbox.m
+++ Lockbox.m
@@ -88,16 +88,14 @@ static NSString *_defaultKeyPrefix = nil;
 
 -(BOOL)setObject:(NSString *)obj forKey:(NSString *)key accessibility:(CFTypeRef)accessibility
 {
-    OSStatus status;
-    
     NSString *hierKey = [self _hierarchicalKey:key];
 
     // If the object is nil, delete the item
     if (!obj) {
         NSMutableDictionary *query = [self _query];
         [query setObject:hierKey forKey:(LOCKBOX_ID)kSecAttrService];
-        status = SecItemDelete((LOCKBOX_DICTREF)query);
-        return (status == errSecSuccess);
+        _lastStatus = SecItemDelete((LOCKBOX_DICTREF)query);
+        return (_lastStatus == errSecSuccess);
     }
     
     NSMutableDictionary *dict = [self _service];
@@ -105,18 +103,18 @@ static NSString *_defaultKeyPrefix = nil;
     [dict setObject: (LOCKBOX_ID)(accessibility) forKey: (LOCKBOX_ID) kSecAttrAccessible];
     [dict setObject: [obj dataUsingEncoding:NSUTF8StringEncoding] forKey: (LOCKBOX_ID) kSecValueData];
     
-    status = SecItemAdd ((LOCKBOX_DICTREF) dict, NULL);
-    if (status == errSecDuplicateItem) {
+    _lastStatus = SecItemAdd ((LOCKBOX_DICTREF) dict, NULL);
+    if (_lastStatus == errSecDuplicateItem) {
         NSMutableDictionary *query = [self _query];
         [query setObject:hierKey forKey:(LOCKBOX_ID)kSecAttrService];
-        status = SecItemDelete((LOCKBOX_DICTREF)query);
-        if (status == errSecSuccess)
-            status = SecItemAdd((LOCKBOX_DICTREF) dict, NULL);
+        _lastStatus = SecItemDelete((LOCKBOX_DICTREF)query);
+        if (_lastStatus == errSecSuccess)
+            _lastStatus = SecItemAdd((LOCKBOX_DICTREF) dict, NULL);
     }
-    if (status != errSecSuccess)
-        DLog(@"SecItemAdd failed for key %@: %d", hierKey, (int)status);
+    if (_lastStatus != errSecSuccess)
+        DLog(@"SecItemAdd failed for key %@: %d", hierKey, (int)_lastStatus);
     
-    return (status == errSecSuccess);
+    return (_lastStatus == errSecSuccess);
 }
 
 -(NSString *)objectForKey:(NSString *)key
@@ -127,10 +125,10 @@ static NSString *_defaultKeyPrefix = nil;
     [query setObject:hierKey forKey: (LOCKBOX_ID)kSecAttrService];
 
     CFDataRef data = nil;
-    OSStatus status =
+    _lastStatus =
     SecItemCopyMatching ( (LOCKBOX_DICTREF) query, (CFTypeRef *) &data );
-    if (status != errSecSuccess && status != errSecItemNotFound)
-        DLog(@"SecItemCopyMatching failed for key %@: %d", hierKey, (int)status);
+    if (_lastStatus != errSecSuccess && _lastStatus != errSecItemNotFound)
+        DLog(@"SecItemCopyMatching failed for key %@: %d", hierKey, (int)_lastStatus);
     
     if (!data)
         return nil;
